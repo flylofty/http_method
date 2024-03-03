837c8cd4ccbbd713aca5a910676c91e727c216fc
lyn <lyn00227@naver.com>
2024-03-03 22:19:31 +0900

    first commit

---
+++ src/main/java/com/method/http/HttpmethodApplication.java
@@ -0,0 +1,13 @@
+package com.method.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpmethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpmethodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/method/http/HttpmethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.method.http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpmethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
